21a245bed473be08faeb860666547f12cc4a5c9d
pberezina <polin.berezina@gmail.com>
2017-10-02 17:09:49 +0300

    change clip function

--- qgis/basicstat_clip.py
+++ qgis/basicstat_clip.py
@@ -1,46 +1,45 @@
-from qgis import *
+import sys
+import os
+from PyQt4.QtGui import *
 from qgis.core import *
 from qgis.utils import *
-from qgis.gui import *
-import processing
-import os
 
- 
-def clipper(directory, mask):
-    file_list = os.listdir(directory)
-    stats = open(directory + 'basicstat.txt', 'w')
-    stats.write('name;features_obl;features_1buff;features_2buff\n')
+app = QApplication([])
+QgsApplication.setPrefixPath("/usr", True)
+QgsApplication.initQgis()
 
-    for file in file_list:
-        if file.endswith('.shp'):
-            layer = QgsVectorLayer(directory + file, file, 'ogr')
-            
-            # set layer crs
-            crs = layer.crs()
-            crs.createFromId(5564)
-            layer.setCrs(crs)
-            
-            # set encoding
-            layer.setProviderEncoding(u'System')
-            layer.dataProvider().setEncoding(u'System')
-            
-            QgsMapLayerRegistry.instance().addMapLayer(layer)
+sys.path.append('/usr/share/qgis/python/plugins')
+from processing.core.Processing import Processing
+from processing.tools import *
+Processing.initialize()
+#from processing.runalg import *
 
-            # clip layer
-            processing.runalg("qgis:clip", directory + file, mask, directory + "clipped/" + file)
-            clippedlayer = QgsVectorLayer(directory + "clipped/" + file, file, 'ogr')
-
-            # count objects in region
-            stats.write(file + ';' + str(layer.getFeatures()) + ';')
-
-            #count clipped objects
-            stats.write(str(clippedlayer.getFeatures()) + ';')
-
-            #count clipped objects
-            stats.write(str(clipped_2layer.getFeatures()) + '\n')
-    stats.close()
-
-
-dir_to_clip = "osm/"
 clip_mask = "area/reg_buff.shp"
-clipper(dir_to_clip, clip_mask)
+dir_path = "osm/"
+
+def clipper(jsonf):
+        layer = QgsVectorLayer(os.path.join(dir_path, jsonf), jsonf.rstrip('.json'), 'ogr')
+
+        # set layer crs
+        crs = layer.crs()
+        crs.createFromId(5564)
+        layer.setCrs(crs)
+
+        # set encoding
+        layer.setProviderEncoding(u'System')
+        layer.dataProvider().setEncoding(u'System')
+
+        QgsMapLayerRegistry.instance().addMapLayer(layer)
+
+        # clip layer
+        output = os.path.join(dir_path, "clipped", jsonf)
+        general.runalg('gdalogr:clipvectorsbypolygon', jsonf, clip_mask, output)  #"qgis:clip"
+        #clippedlayer = QgsVectorLayer(directory + "clipped/" + file, file, 'ogr')
+
+
+
+for fname in os.listdir(dir_path):
+    if fname.endswith('.shp')
+        clipper(fname)
+
+QgsApplication.exitQgis()
